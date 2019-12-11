556d3f43d2ff133e43f6e36bae0fbc604afbb48d
Lucy Weltner <lucyiweltner@gmail.com>
2019-12-11 23:40:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
   attr_accessor :name, :grade
   attr_reader :id
-  def initialize(id = nil, grade=9, name="Joe")
+  def initialize(grade=9, name="Joe", id = nil)
     @name = name
     @grade = grade
     @id = id
